e7f491a0daeeb82b197f9501059179c2163de2d6
suaq <s8186255@163.com>
2015-07-01 19:27:32 +0800

    测试代码

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -5,8 +5,33 @@ class Api::BooksController < ApiController
   # GET /api/books.json
   def index
     @api_books = Api::Book.all
+    url1='http://api.open189.net:10035/xj/as/dial'
+    url2='http://api.open189.net:10035/xj/as/meetstart?'
+    timestamp = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    access_token = '01b8d3d6d16a44af834f1a9f8b1ae28d'
+    token_secret = '9e2de3b33c4021d77913faa89a0f92e4'
+    sign= Digest::MD5.digest(access_token + timestamp + token_secret).unpack("H*")[0]
+    str_params1 = %Q{
+    access_token=938d2bdcb0f750ab8720901b66df67e31393902726651&
+    timestamp=#{timestamp}&auth_token=&
+    format=JSON&state=&sign=#{sign}&
+    req_body=<vChargeNbr>09913676865</vChargeNbr><vDisplayNbr>09913676865</vDisplayNbr><vCallerNbr>09913676865</vCallerNbr><vCalledNbr>15309910500</vCalledNbr>
+    }
 
-    render json: @api_books
+    str_params2 = "access_token=#{access_token}&timestamp=#{timestamp}&auth_token=&format=JSON&state=&sign=#{sign}&req_body=<vDisplayNbr>09913676865</vDisplayNbr><vAdminNbr>09913676865</vAdminNbr><vMemberNbr>15309910500|18099209001</vMemberNbr>"
+
+
+    json_params = {
+        access_token: access_token,
+        timestamp: timestamp,
+        sign: Digest::MD5.digest(access_token + timestamp + token_secret).unpack("H*")[0],
+        req_body: "<vDisplayNbr>09913676865</vDisplayNbr><vCallerNbr>09913676865</vCallerNbr><vCalledNbr>15309910500</vCalledNbr><vRecord>0</vRecord>"
+
+    }
+    #render json: @api_books
+    RestClient.get url2+str_params2
+    RestClient.post url1,json_params
+    #Net::HTTP.post_form URI.parse(url1),json_params
   end
 
   # GET /api/books/1
@@ -49,11 +74,11 @@ class Api::BooksController < ApiController
 
   private
 
-    def set_api_book
-      @api_book = Api::Book.find(params[:id])
-    end
+  def set_api_book
+    @api_book = Api::Book.find(params[:id])
+  end
 
-    def api_book_params
-      params.require(:api_book).permit(:name, :author)
-    end
+  def api_book_params
+    params.require(:api_book).permit(:name, :author)
+  end
 end
