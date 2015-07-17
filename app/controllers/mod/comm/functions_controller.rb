31b97e5d665e9276972c2911ece6e6815372294f
suaq <s8186255@163.com>
2015-07-17 17:49:01 +0800

    增加通信模块

--- app/controllers/mod/comm/functions_controller.rb
+++ app/controllers/mod/comm/functions_controller.rb
@@ -1,4 +1,4 @@
-class Mod::Comm::ApiController < ApiController
+class Mod::Comm::FunctionsController < Mod::Comm::BaseController
   before_action :set_mod_comm_api, only: [:show, :update, :destroy]
 
   def meeting
@@ -7,24 +7,36 @@ class Mod::Comm::ApiController < ApiController
     #第二种参数：一个组的名称；
 
     #将这组电话，发送到后台，形成呼叫；
+    phones = params[:phones]
+    phone_group = params[:phone_group]
 
+    unless phones.blank?
+
+    end
+
+    unless phone_group.blank?
+
+    end
   end
+
   def record
     #参数说明
 
   end
+
   def message
     #参数说明
 
   end
 
+
   private
 
-    def set_mod_comm_api
-      @mod_comm_api = Mod::Comm::Api.find(params[:id])
-    end
+  def set_mod_comm_api
+    @mod_comm_api = Mod::Comm::Api.find(params[:id])
+  end
 
-    def mod_comm_api_params
-      params[:mod_comm_api]
-    end
+  def mod_comm_api_params
+    params[:mod_comm_api]
+  end
 end
